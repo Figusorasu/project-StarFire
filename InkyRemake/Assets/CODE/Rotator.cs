fc602c08c6fb6da7d6447cb19e632038e2cd24ed
Figus <99464311+Figusorasu@users.noreply.github.com>
2023-04-07 13:52:48 +0200

    v0.1.2.1d
    + Rotator script

---
+++ InkyRemake/Assets/CODE/Rotator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Header("Rotation Settings")]
+    [SerializeField] private float minRotationSpeed = 0f;
+    [SerializeField] private float maxRotationSpeed;
+
+    [SerializeField] private float rotateSpeed;
+
+    private enum RotationType{Horizontal, Vertical, Round}
+    [SerializeField] private RotationType rotationType; 
+    private enum RotationDirection{Right, Left}
+    [SerializeField] private RotationDirection rotationDirection;
+
+
+    private void Update()
+    {
+        switch (rotationType)
+        {
+            case RotationType.Horizontal:
+                if(rotationDirection == RotationDirection.Right) {
+                    transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self);
+                } else {
+                    transform.Rotate(Vector3.up * -rotateSpeed * Time.deltaTime, Space.Self);
+                }
+            break;
+            case RotationType.Vertical:
+                if(rotationDirection == RotationDirection.Right) {
+                    transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime, Space.Self);
+                } else {
+                    transform.Rotate(Vector3.right * -rotateSpeed * Time.deltaTime, Space.Self);
+                }
+            break;
+            case RotationType.Round:
+                if(rotationDirection == RotationDirection.Right) {
+                    transform.Rotate(Vector3.forward * -rotateSpeed * Time.deltaTime, Space.Self);
+                } else {
+                    transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime, Space.Self);
+                }
+            break;
+        }
+    }
+}
