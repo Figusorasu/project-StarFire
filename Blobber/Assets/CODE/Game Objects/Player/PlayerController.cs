0b1b83063748d35b1f513983d9fd6ba35d1869a1
Figus <99464311+Figusorasu@users.noreply.github.com>
2023-04-13 02:07:19 +0200

    d0.1.6

--- Blobber/Assets/CODE/Game Objects/Player/PlayerController.cs
+++ Blobber/Assets/CODE/Game Objects/Player/PlayerController.cs
@@ -9,74 +9,97 @@ public class PlayerController : MonoBehaviour
 {   
     #region Variables
 
-    [Header("Movement, Jump & Ground Detection")]
-    public float speed;
-    public float jumpForce;
-    public float dashForce;
-    public float checkRadius;
+        [Header("Movement")]
+            public float speed;
+            public float jumpForce;
+            public float dashForce;
 
-    [HideInInspector] public bool canMove = true;
-    [HideInInspector] public bool canJump = false;
-    [HideInInspector] public bool canDubbleJump = false;
-    [HideInInspector] public bool isGrounded;
+            [HideInInspector]
+            public bool canMove = true, canJump = false, canDubbleJump = false;
 
-    [SerializeField] private LayerMask whatIsGround;
+        [Header("Ground Detection")]
+            public float checkRadius;
+            [HideInInspector] public bool isGrounded;
 
-    private float inputHorizontal;
-    private bool facingRight = true;
+            [Space, SerializeField] private LayerMask whatIsGround;
+            private float inputHorizontal;
+            private bool facingRight = true;
 
-    [Header("Stats")]
-    public int health;
-    public int numOfHearts;
+        [Header("Stats")]
+            public int health;
+            public int numOfHearts;
 
-    [Header("Components")]
-    [InspectorName("Rigidbody2D")] public Rigidbody2D rb;
+        [Header("Components")]
+            [SerializeField] private Rigidbody2D _rb;
+            [SerializeField] private Transform _groundCheck;
+            [SerializeField] private Animator _anim;
 
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private Animator anim;
-    [SerializeField] private PlayerInput input;
-
-    private GameManager GM;
+            private GameManager _GM;
+            private InputManager _inputControls;
 
     #endregion
     //=======================================================================================//
-    #region Unity Functions
+    #region InputHandler
+
+        private void Awake() {
+            _inputControls = new InputManager();
+
+            _inputControls.Player.Move.performed += ctx => inputHorizontal = ctx.ReadValue<Vector2>().x;
+            _inputControls.Player.Move.canceled += ctx => inputHorizontal = 0;
+
+            _inputControls.Player.Jump.performed += ctx => Jump();
+            _inputControls.Player.Dash.performed += ctx => Dash();
+        }
+
+        private void Jump() {
+            if(canJump){
+                _rb.velocity = Vector2.up * jumpForce * Time.deltaTime;
+            }
+        }
+
+        private void Dash() {
+            Debug.Log("Dash!");
+        }
+
+        private void OnEnable() {
+            _inputControls.Player.Enable();
+        }
+        private void OnDisable() {
+            _inputControls.Player.Disable();
+        }
+
+    #endregion
 
     private void Start() {
-        GM = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
+        _GM = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
     }
 
     private void Update() {
-        if(isGrounded) {
-            canJump = true;
-        } else {
-            canJump = false;
-        }
+        
+        canJump = isGrounded;
 
         // ANIMATIONS
-        if(rb.velocity.x == 0) {
-            anim.SetBool("isMoving", false);
+        if(_rb.velocity.x == 0) {
+            _anim.SetBool("isMoving", false);
         } else {
-            anim.SetBool("isMoving", true);
+            _anim.SetBool("isMoving", true);
         }
-        anim.SetBool("isGrounded", isGrounded);
+        _anim.SetBool("isGrounded", isGrounded);
 
     }
 
     private void FixedUpdate() {
         if(canMove) {
-            rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+            _rb.velocity = new Vector2(inputHorizontal * speed, _rb.velocity.y) * Time.deltaTime;
         }
 
-        if(!facingRight && rb.velocity.x > 0) {
+        if(!facingRight && _rb.velocity.x > 0) {
             Flip();
-        } else if(facingRight && rb.velocity.x < 0) {
+        } else if(facingRight && _rb.velocity.x < 0) {
             Flip();
         }
 
-
-
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(_groundCheck.position, checkRadius, whatIsGround);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -88,53 +111,8 @@ public class PlayerController : MonoBehaviour
     private void OnDrawGizmosSelected() {
         // Ground Check Radius Display
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+        Gizmos.DrawWireSphere(_groundCheck.position, checkRadius);
     }
-
-    #endregion
-    //=======================================================================================//
-    #region Input System
-
-    private void OnEnable() {
-        input.onActionTriggered += PlayerInputOnActionTriggered;
-    }
-
-    private void PlayerInputOnActionTriggered(InputAction.CallbackContext ctx) {
-
-        if(ctx.action.name == "Move") {
-            inputHorizontal = ctx.ReadValue<Vector2>().x;
-        }
-        
-        if(ctx.action.name == "Jump") {
-            rb.velocity = Vector2.up * jumpForce;
-        }
-
-        if(ctx.action.name == "Dash") {
-            rb.velocity = Vector2.right * dashForce;
-        }
-    }
-
-
-    /* 
-    public void OnMove(InputAction.CallbackContext ctx) {
-        inputHorizontal = ctx.ReadValue<Vector2>().x;
-    }
-
-    public void OnJump(InputAction.CallbackContext ctx) {
-        if(ctx.performed && canJump) {
-            rb.velocity = Vector2.up * jumpForce;
-        }
-    }
-
-    public void OnDash(InputAction.CallbackContext ctx) {
-        if(ctx.performed) {
-            rb.velocity = Vector2.right * dashForce;
-        }
-    }*/
-
-    #endregion
-    //=======================================================================================//
-    #region Custom Methods
 
     private void Flip() {
         facingRight = !facingRight;
@@ -143,7 +121,4 @@ public class PlayerController : MonoBehaviour
         transform.localScale = Scaler;
     }
 
-
-
-    #endregion
 }
