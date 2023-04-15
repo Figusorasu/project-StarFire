c890600085dbe9f1d5b47ef4a067082a5d4212e1
Figus <99464311+Figusorasu@users.noreply.github.com>
2023-04-15 12:04:21 +0200

    d0.1.11

--- Blobber/Assets/CODE/UI Scripts/GameHUD.cs
+++ Blobber/Assets/CODE/UI Scripts/GameHUD.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameHUD : MonoBehaviour
 {
     [SerializeField] private GameObject debugInfo;
-    private bool isActive = true;
+    private bool isActive = false;
 
     private void Update() {
         debugInfo.SetActive(isActive);
